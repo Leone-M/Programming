ba0e0d7a2b566cf1de3ec93e0697cc5f0840ed48
cH4F5 <hotroddustbit@gmail.com>
2022-10-22 20:38:36 +0300

    Ура победа над 5.3

---
+++ 5 лекция/3.py
@@ -0,0 +1,53 @@
+def main():
+    stroka = str(input())
+
+    i = 0
+    allow_1 = 0
+    allow_2 = 0
+    allow_3 = 0
+
+    podstroka = ""
+    max_podstroka = ""
+    maxx = 0
+
+    while i != len(stroka):
+        if stroka[i] == "(":
+            allow_1 += 1
+            podstroka += stroka[i]
+        elif stroka[i] == "[":
+            allow_2 += 1
+            podstroka += stroka[i]
+        elif stroka[i] == "{":
+            allow_3 += 1
+            podstroka += stroka[i]
+        elif stroka[i] == ")" and allow_1 > 0 and not (stroka[i - 1] == "[" or stroka[i - 1] == "{"):
+            podstroka += stroka[i]
+            allow_1 -= 1
+        elif stroka[i] == "]" and allow_2 > 0 and not (stroka[i - 1] == "(" or stroka[i - 1] == "{"):
+            podstroka += stroka[i]
+            allow_2 -= 1
+        elif stroka[i] == "}" and allow_3 > 0 and not (stroka[i - 1] == "[" or stroka[i - 1] == "("):
+            podstroka += stroka[i]
+            allow_3 -= 1
+        else:
+            allow_1 = 0
+            allow_2 = 0
+            allow_3 = 0
+            maxx = 0
+            podstroka = ""
+
+        if len(podstroka) > maxx:
+            maxx += 1
+            max_podstroka = podstroka
+
+        i += 1
+
+    if max_podstroka == stroka:
+        print(True)
+    elif max_podstroka != "":
+        print(max_podstroka)
+    else:
+        print(False)
+
+if __name__ == "__main__":
+    main()
