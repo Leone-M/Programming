b728ab21e2cbfb01f3907ba6b3533253bc6ca80b
cH4F5 <hotroddustbit@gmail.com>
2022-10-29 12:55:11 +0300

    Парарам

---
+++ 9 лекция/9_3rec.py
@@ -0,0 +1,14 @@
+import itertools
+
+buf = [[5, 7, 8, 9, 0], [5, 7, 8, 9, 0]]
+
+spisok = []
+subcode = ""
+flags = [0] * len(buf)
+j = len(buf) - 1
+
+for i in range(len(buf)):
+    for j in range()
+
+
+#fu = list(itertools.combinations(buf, len(buf)))
